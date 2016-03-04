013a3e1bb6f8d1f7ecc350a6806c6514b388fb80
Pedro Volpi <pedrovolpi@poli.ufrj.br>
2016-03-04 01:19:43 -0300

    Commit 04/03/2016

--- Projeto/enquetes/views.py
+++ Projeto/enquetes/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+	return HttpResponse("Hello world. Pagina de enquetes")
+def detail(request, question_id):
+	response = "Você está olhando os resultados da enquete %s"
+	return HttpResponse(response % question_id)
+def results(request, question_id):
+	response = "Você está olhando os resultados da enquete %S"
+	return HttpResponse(response % question_id)
+def vote(request, question_id):
+	response = "Você está votando na enquete %s"
+	return HttpResponse(response % question_id)
