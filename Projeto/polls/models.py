5e3a2753407a22cde4dc4ad127ef3d42b556cb90
MMaestrelliLeobons <mml@globo.com>
2016-03-11 23:43:46 -0300

    Add tests. Add utf-8 enconding to all files.

--- Projeto/polls/models.py
+++ Projeto/polls/models.py
@@ -1,10 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('data de publicação')
 	def __str__(self):
 		return self.question_text
+	def was_published_recently(self):
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question)
--- Projeto/polls/views.py
+++ Projeto/polls/views.py
@@ -1,8 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.views import generic
 from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -16,6 +20,9 @@ def index(request):
 class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
+
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
 	model = Question
@@ -34,3 +41,10 @@ def vote(request, question_id):
 		selected_choice.votes += 1
 		selected_choice.save()
 		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
