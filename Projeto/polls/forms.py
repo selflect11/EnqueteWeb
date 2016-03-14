bc2aa35fa66f11054ac7c7a6fc831fb445ad6e60
Pedro Volpi <pedrovolpi@poli.ufrj.br>
2016-03-13 22:45:27 -0300

    final

--- Projeto/polls/forms.py
+++ Projeto/polls/forms.py
@@ -3,5 +3,9 @@ from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
 	class Meta:
+		password = forms.CharField(widget=forms.PasswordInput)
 		model = User
 		fields = '__all__'
+		widgets = {
+			'password': forms.PasswordInput(),
+		}
